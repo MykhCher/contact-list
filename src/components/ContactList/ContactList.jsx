603f65bf78323c5bf26c5a3f40f6af36e7fcb250
MykhCher <micher1240@gmail.com>
2024-05-28 14:00:42 +0300

    add components

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+export class ContactList extends Component {
+  render() {
+    return (
+      <div>ContactList</div>
+    )
+  }
+}
+
+export default ContactList
